0e5f2d50376b03fdfa1f985032b72df76e2fdce9
Dan Nusinovich <dann@Dan-Nusinovich-Mac.local>
2023-05-24 22:59:44 -0700

    Added 404 route

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
+
 const express = require ('express')
 app get ('/', function (req, res){
 res.send('Welcome to Page 1!')
 })
 
-app.listen(3000, function(){
-    console.log('Server is up and running!')
+app get ('*', function (req, res){
+    res.status(404).send('<h1>404 Error, page in not available</h1>')
 })
+
+app.listen(process.env.PORT)
