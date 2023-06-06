e2c318c15f7c65b7f3138b9348548bc30f8af620
Dan Nusinovich <dann@Dan-Nusinovich-Mac.local>
2023-06-05 20:28:54 -0700

    complete part 3

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,28 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{data.place.name}</h1>
           </main>
+          <div>
+          <img src={data.place.pic} alt={data.place.name} />
+          </div>
+          <section>
+            <h2>Rating</h2>
+            <p>currently unrated</p>
+          </section>
+          <section>
+            <h2>Description</h2>
+            <p>Located in {data.place.city}, {data.place.state}</p>
+            
+          </section>
+          <section>
+            <h2>Comments</h2>
+            <p>No comments yet!</p>
+          </section>
         </Def>
     )
 }
