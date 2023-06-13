7e29f761b4ab9f2a483d40b2985721898ed327d3
Dan Nusinovich <dann@Dan-Nusinovich-Mac.local>
2023-06-12 18:17:01 -0700

    change edit.jsx

--- models/places.js
+++ models/places.js
@@ -6,8 +6,8 @@ const placeSchema = new Schema({
     name: {type: String, require: true},
     pic: {type: String, default: "https://cdn.vectorstock.com/i/preview-1x/82/99/no-image-available-like-missing-picture-vector-43938299.jpg"},
     cuisines: {type: String, require: true},
-    city: {type: String, Default: "Anytown"},
-    state: {type: String, Default: "USA"},
+    city: {type: String, default: "Anytown"},
+    state: {type: String, default: "USA"},
     founded: Number
 })
 
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -7,27 +7,29 @@ function edit_form (data) {
         <main>
             <h1> Edit Place Information</h1>
             <form method='POST' action={`/places/${data.id}?_method=PUT`}>
-                <div className='form-group'>
-                    <label htmlFor='name' > Business Name</label><br></br>
-                    <input className='form-control' id='name' name='name' value={data.place.name} required></input>
-                </div><br></br>
-                <div className='form-group'>
-                    <label htmlFor='pic'>Place Business Picture</label><br></br>
-                    <input className='form-control' type='url' id='pic' name='pic' value={data.place.pic}></input>
-                </div><br></br>
-                <div className='form-group'>
-                    <label htmlFor='city'>City</label><br></br>
-                    <input className='form-control' id='city' name='city' value={data.place.city}></input>
-                </div><br></br>
-                <div className='form-group'>
-                    <label htmlFor='state'>State</label><br></br>
-                    <input className='form-control' id='state' name='state' value={data.place.state}></input>
-                </div><br></br>
-                <div className='form-group'>
-                    <label htmlFor='cuisines'>Cuisines</label><br></br>
-                    <input className='form-control' id='cuisines' name='cuisines' value={data.place.cuisines} required></input>
-                </div><br></br>
-                <input className='btn btn-primary' type='submit' value='Edit Information'></input>
+                <div className='row'>
+                    <div className='col-sm-6 col-md-4 col-lg-3'>
+                        <label htmlFor='name' > Business Name</label><br></br>
+                        <input className='form-control' id='name' name='name' value={data.place.name} required></input>
+                    </div><br></br>
+                    <div className='col-sm-6 col-md-4 col-lg-3'>
+                        <label htmlFor='pic'>Place Business Picture</label><br></br>
+                        <input className='form-control' type='url' id='pic' name='pic' value={data.place.pic}></input>
+                    </div><br></br>
+                    <div className='col-sm-6 col-md-4 col-lg-3'>
+                        <label htmlFor='city'>City</label><br></br>
+                        <input className='form-control' id='city' name='city' value={data.place.city}></input>
+                    </div><br></br>
+                    <div className='col-sm-6 col-md-4 col-lg-3'>
+                        <label htmlFor='state'>State</label><br></br>
+                        <input className='form-control' id='state' name='state' value={data.place.state}></input>
+                    </div><br></br>
+                    <div className='col-sm-6 col-md-4 col-lg-3'>
+                        <label htmlFor='cuisines'>Cuisines</label><br></br>
+                        <input className='form-control' id='cuisines' name='cuisines' value={data.place.cuisines} required></input>
+                    </div><br></br>
+                    <input className='btn btn-primary' type='submit' value='Edit Information'></input>
+                </div>  
             </form>
         </main>
     </Def>
