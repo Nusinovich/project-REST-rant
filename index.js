54b06aa86e59f3bc18f8608cac1ea20730ac2f43
Dan Nusinovich <dann@Dan-Nusinovich-Mac.local>
2023-06-26 00:26:36 +0100

    fix few issue before deployment

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const express = require ('express')
 const app = express()
 const methodOverride = require('method-override')
+const  mongoose = require('mongoose')
 
-app.use(methodOverride('_method'))
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use ('/places', require('./controllers/places'))
@@ -15,6 +17,11 @@ app.get ('/', function (req, res){
 res.render('Home')
 })
 
+
+//mongoose connection
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, ()=> {
+    console.log('connectd to Mongo DB:', process.env.MONGO_URI)
+})
 
 app.get ('*', function (req, res){
 res.render('error404')
